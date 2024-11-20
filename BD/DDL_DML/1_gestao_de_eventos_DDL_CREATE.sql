8ce27d1e30c74702ddf051cbd8dd458a624b60f0
TH3-C0DE1 <marciopp1995@hotmail.com>
2024-11-20 01:24:22 +0000

    Atualização dos CREATE TABLES

--- BD/DDL_DML/1_gestao_de_eventos_DDL_CREATE.sql
+++ BD/DDL_DML/1_gestao_de_eventos_DDL_CREATE.sql
@@ -4,58 +4,20 @@
 CREATE DATABASE event_management;
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Permissions */
+/* CREATE TABLE - Users Type */
 
-CREATE TABLE permissions (
+CREATE TABLE users_type (
     id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    desc VARCHAR(250),
-    PRIMARY KEY (id));
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Admin Type */
-
-CREATE TABLE admin_type (
-    id INT AUTO_INCREMENT,
-    admin_type VARCHAR(250),
+    users_type VARCHAR(250),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Admin Permissions */
+/* CREATE TABLE - Users Address */
 
-CREATE TABLE admin_permissions (
-    admin_type_id INT,
-    permission_id INT,
-    PRIMARY KEY (admin_type_id, permission_id),
-    CONSTRAINT admin_permissions_fk1
-            FOREIGN KEY (admin_type_id) REFERENCES admin_type (id),
-            FOREIGN KEY (permission_id) REFERENCES permissions (id));
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Admin User */
-
-CREATE TABLE admin_user (
-    id INT AUTO_INCREMENT,
-    user_name VARCHAR(100) NOT NULL,
-    user_password VARCHAR(250) NOT NULL,
-    first_name VARCHAR(100),    
-    last_name VARCHAR(100),
-    email VARCHAR(250),
-    type_id INT,
-    last_login DATETIME,
-    status ENUM('active', 'inactive', 'suspended') DEFAULT 'active',
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    CONSTRAINT type_id_fk1
-		    FOREIGN KEY (type_id) REFERENCES admin_type (id) );
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - User Address */
-
-CREATE TABLE user_address (
+CREATE TABLE users_address (
     id INT AUTO_INCREMENT,
     address_line1 VARCHAR(250),
     address_line2 VARCHAR(250),
@@ -66,9 +28,9 @@ CREATE TABLE user_address (
     PRIMARY KEY (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - User */
+/* CREATE TABLE - Users */
 
-CREATE TABLE user (
+CREATE TABLE users (
     id INT AUTO_INCREMENT,
     user_name VARCHAR(100) NOT NULL,
     user_password VARCHAR(250) NOT NULL,
@@ -81,34 +43,33 @@ CREATE TABLE user (
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT address_id_fk1
-		    FOREIGN KEY (address_id) REFERENCES user_address (id) );
+    CONSTRAINT address_fk1
+		    FOREIGN KEY (address_id) REFERENCES users_address (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - User Payment */
+/* CREATE TABLE - Users Type / Users */
 
-CREATE TABLE user_payment (
+CREATE TABLE users_type_users (
+    users_id INT,
+    users_type_id INT,
+    PRIMARY KEY (users_id, users_type_id),
+    CONSTRAINT users_type_users_fk1
+            FOREIGN KEY (users_id) REFERENCES users (id),
+            FOREIGN KEY (users_type_id) REFERENCES users_type (id));
+
+/* ----------------------------------------------------------------------------------------------------------------- */
+/* CREATE TABLE - Users Payments */
+
+CREATE TABLE users_payments (
     id INT AUTO_INCREMENT,
-    user_id INT,
+    users_id INT,
     payment_type VARCHAR(100),
-    provider VARCHAR(100),
-    expiry DATE,
+    payment_provider VARCHAR(100),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT user_id_fk1
-		    FOREIGN KEY (user_id) REFERENCES user (id) );
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Events Location */
-
-CREATE TABLE events_location (
-    id INT AUTO_INCREMENT,
-    address_line1 VARCHAR(250),
-    address_line2 VARCHAR(250),
-    postal_code VARCHAR(20),
-    city VARCHAR(50),
-    region VARCHAR(50),
-    country VARCHAR(50),
-    PRIMARY KEY (id) );
+    CONSTRAINT users_fk1
+		    FOREIGN KEY (users_id) REFERENCES users (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
 /* CREATE TABLE - Events Category */
@@ -132,24 +93,27 @@ CREATE TABLE events (
     cover VARCHAR(250),       /* IMAGE - BLOB Type ??? */
     d_day DATETIME,
     capacity INT,
-    location_id INT,
+    address_line1 VARCHAR(250),
+    address_line2 VARCHAR(250),
+    postal_code VARCHAR(20),
+    city VARCHAR(50),
+    region VARCHAR(50),
+    country VARCHAR(50),
     category_id INT,
     status ENUM('Active', 'Scheduled', 'Cancelled') DEFAULT 'Scheduled',
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT location_id_fk1
-        FOREIGN KEY (location_id) REFERENCES events_location (id),
-    CONSTRAINT category_id_fk2
+    CONSTRAINT category_fk1
         FOREIGN KEY (category_id) REFERENCES events_category (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Ticket Type */
+/* CREATE TABLE - Tickets Type */
 
-CREATE TABLE ticket_type (
+CREATE TABLE tickets_type (
     id INT AUTO_INCREMENT,
-    type VARCHAR(100),
+    tickets_type VARCHAR(100),
     desc VARCHAR(250),
     status ENUM('Active', 'Disabled') DEFAULT 'Active',
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -158,12 +122,12 @@ CREATE TABLE ticket_type (
     PRIMARY KEY (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Events Info */
+/* CREATE TABLE - Tickets Info */
 
-CREATE TABLE events_info (
+CREATE TABLE tickets_info (
     id INT AUTO_INCREMENT,
-    event_id INT,
-    type_id INT,
+    events_id INT,
+    tickets_type_id INT,
     SKU VARCHAR(250),
     price DECIMAL(10, 2),
     quantity INT,
@@ -172,53 +136,57 @@ CREATE TABLE events_info (
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     deleted TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT event_id_fk1
-		    FOREIGN KEY (event_id) REFERENCES events (id),
-    CONSTRAINT type_id_fk2
-		    FOREIGN KEY (type_id) REFERENCES ticket_type (id) );
+    CONSTRAINT events_fk1
+		    FOREIGN KEY (events_id) REFERENCES events (id),
+    CONSTRAINT tickets_type_fk2
+		    FOREIGN KEY (tickets_type_id) REFERENCES tickets_type (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Shopping Session */
+/* CREATE TABLE - Ordered Tickets */
 
-CREATE TABLE shopping_session (
+CREATE TABLE ordered_tickets (
     id INT AUTO_INCREMENT,
-    user_id INT,
-    total DECIMAL(10, 2),
-    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    CONSTRAINT user_id_fk2
-		    FOREIGN KEY (user_id) REFERENCES user (id) );
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Cart Item */
-
-CREATE TABLE cart_item (
-    id INT AUTO_INCREMENT,
-    session_id INT,
-    events_info_id INT,
+    tickets_info_id INT,
     quantity INT,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    CONSTRAINT session_id_fk1
-		    FOREIGN KEY (session_id) REFERENCES shopping_session (id),
-    CONSTRAINT events_info_id_fk1
-		    FOREIGN KEY (events_info_id) REFERENCES events_info (id) );
-
-/* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Payment Details */
-
-
+    CONSTRAINT events_fk2
+		    FOREIGN KEY (events_id) REFERENCES events (id), );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
 /* CREATE TABLE - Order Details */
 
-
+CREATE TABLE order_details (
+    id INT AUTO_INCREMENT,
+    users_id INT,
+    ordered_tickets_id INT,
+    order_total DECIMAL(10, 2),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    on_cart ENUM('Active', 'Scheduled', 'Cancelled') DEFAULT 'Scheduled',
+    status ENUM('Pending', 'Completed', 'Cancelled') DEFAULT 'Pending',
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    CONSTRAINT users_fk2
+		    FOREIGN KEY (users_id) REFERENCES users (id),
+    CONSTRAINT ordered_tickets_fk2
+		    FOREIGN KEY (ordered_tickets_id) REFERENCES ordered_tickets (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
-/* CREATE TABLE - Order Ticket */
+/* CREATE TABLE - Payment Details */
 
-
+CREATE TABLE payment_details (
+    id INT AUTO_INCREMENT,
+    order_id INT,
+    users_payments_id INT,
+    payment_amount DECIMAL(10, 2),
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('Paid', 'Failed', 'Refunded') DEFAULT 'Paid',
+    PRIMARY KEY (id),
+    CONSTRAINT order_fk1
+		    FOREIGN KEY (order_id) REFERENCES order (id),
+    CONSTRAINT users_payments_fk2
+		    FOREIGN KEY (users_payments_id) REFERENCES users_payments (id) );
 
 /* ----------------------------------------------------------------------------------------------------------------- */
