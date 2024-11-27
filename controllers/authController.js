8f200bbd9680f2d77b26fd2cc66757288ea30f79
joaolado <joaocarmo@ipvc.pt>
2024-11-27 16:39:08 +0000

    Login (a funcionar )

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,23 @@ const registerUser = async (req, res) => {
     }
 };
 
+// User login
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (user && await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.json({ token });
+        } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(400).json({ error: 'Failed to login.', details: error.message });
+    }
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
