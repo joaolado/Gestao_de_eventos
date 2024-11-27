fadcf7e4a9145d1e239992e78f6e21a2b461aabf
TH3-C0DE1 <marciopp1995@hotmail.com>
2024-11-27 18:52:27 +0000

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,8 +3,9 @@ const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-// User registration
-const registerUser = async (req, res) => {
+// User Registration
+const registerUser = async (req, res) => 
+{
     const 
     { 
         email, 
@@ -12,33 +13,62 @@ const registerUser = async (req, res) => {
 
     } = req.body;
     
-    try {
+    try 
+    {
+        // User bcrypt Password - Max. 10 Char
         const hashedPassword = await bcrypt.hash(userPassword, 10);
+
         const user = await prisma.users.create({
+
             data: 
             {
-                email,
+                email: email,
                 userPassword: hashedPassword,
             },
         });
+
+        // Successful Registration
         res.status(201).json(user);
-    } catch (error) {
-        res.status(400).json({ error: 'Failed to Register user.', details: error.message });
+    } 
+
+    catch (error) 
+    {
+        res.status(400).json({ error: 'Failed to Register User.', details: error.message });
     }
 };
 
-// User login
-const loginUser = async (req, res) => {
-    const { email, userPassword } = req.body;
-    try {
+// User Login
+const loginUser = async (req, res) => 
+{
+    const 
+    { 
+        email, 
+        userPassword 
+
+    } = req.body;
+
+    try 
+    {
         const users = await prisma.users.findUnique({ where: { email } });
-        if (users && await bcrypt.compare(userPassword, users.userPassword)) {
+
+        if (users && await bcrypt.compare(userPassword, users.userPassword)) 
+        {
+            // Token Expiration Time
             const token = jwt.sign({ usersId: users.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+            // Successful Login
+            res.status(200).json({ message: 'Successful Login.' });
             res.json({ token });
-        } else {
-            res.status(401).json({ error: 'Invalid credentials' });
         }
-    } catch (error) {
+
+        else 
+        {
+            res.status(401).json({ error: 'Invalid Credentials.' });
+        }
+    }
+
+    catch (error) 
+    {
         res.status(400).json({ error: 'Failed to Login.', details: error.message });
     }
 };
