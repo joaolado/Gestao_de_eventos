e93e7b36f4b880bac510af18fd5590c133082bfb
TH3-C0DE1 <marciopp1995@hotmail.com>
2024-11-28 19:02:05 +0000

    add profile pic

--- routes/v1/r_Events.js
+++ routes/v1/r_Events.js
@@ -2,7 +2,7 @@
 const eventsRouter = require('express').Router();
 const authenticateToken = require('../../middlewares/authMiddleware'); // Import the JWT auth Middleware
 const authorizeRole = require('../../middlewares/authorizeRole');      // Import authorizeRole
-const upload = require('../../middlewares/uploadMiddleware');          // Import Multer Upload Middleware
+const upload = require('../../middlewares/uploadCoversMiddleware');    // Import Multer Upload Middleware
 const controller = require('../../controllers/v1/c_Events');
 
 // Events CRUD
@@ -11,8 +11,8 @@ eventsRouter.get('/', authenticateToken, controller.getAll);                    
 eventsRouter.get('/:id', authenticateToken, controller.getById);                                                                            // Get a Events by ID
 
 // Protected Routes (Authentication Required - Token)
-eventsRouter.post('/create', authenticateToken, authorizeRole(['UserAdmin', 'UserSuperAdmin']), upload.single('cover'), controller.create);                         // Create a new Events
-eventsRouter.put('/update', authenticateToken, authorizeRole(['UserAdmin', 'UserSuperAdmin']), upload.single('cover'), controller.update);                          // Update a Events
+eventsRouter.post('/create', authenticateToken, authorizeRole(['UserAdmin', 'UserSuperAdmin']), upload.single('cover'), controller.create); // Create a new Events
+eventsRouter.put('/update', authenticateToken, authorizeRole(['UserAdmin', 'UserSuperAdmin']), upload.single('cover'), controller.update);  // Update a Events
 eventsRouter.delete('/delete/:id', authenticateToken, authenticateToken, authorizeRole(['UserSuperAdmin']), controller.delete);             // Delete a Events by ID
 
 // PATCH Used for Parcial Update (Authentication Required - Token)
